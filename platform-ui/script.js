50fe4ccdc48509f99d92ae620a2637ef34742850
HarshaKatta18 <harshakatta18@gmail.com>
2026-05-06 10:39:06 +0530

    remove token from UI

--- platform-ui/script.js
+++ platform-ui/script.js
@@ -1,3 +1,36 @@
-function deploy(template) {
-  alert("Deploying " + template + "...\n(Connect backend next)");
+async function deploy(template) {
+
+  const token = prompt("Enter GitHub Token");
+  const repo = "HarshaKatta18/devflow-idp"; // your repo
+
+  alert("Deploying " + template + "...");
+
+  try {
+    const response = await fetch(
+      `https://api.github.com/repos/${repo}/actions/workflows/deploy.yml/dispatches`,
+      {
+        method: "POST",
+        headers: {
+          "Accept": "application/vnd.github+json",
+          "Authorization": "Bearer " + token
+        },
+        body: JSON.stringify({
+          ref: "main",
+          inputs: {
+            template: template
+          }
+        })
+      }
+    );
+
+    if (response.status === 204) {
+      alert("✅ Deployment triggered! Check GitHub Actions");
+    } else {
+      const text = await response.text();
+      alert("❌ Failed: " + text);
+    }
+
+  } catch (err) {
+    alert("❌ Error: " + err.message);
+  }
 }
